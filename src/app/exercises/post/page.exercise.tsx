e561a33d0af2968aee6385a2207b6e56e5135dc3
tloyan <thomas.loyan@gmail.com>
2024-11-14 13:44:30 +0100

    feature/fetch-rsc

--- src/app/exercises/post/page.exercise.tsx
+++ src/app/exercises/post/page.exercise.tsx
@@ -1,40 +1,16 @@
-'use client'
+'use server'
 import {Post} from '@/lib/type'
-// ⛏️ Supprime `useEffect` et `useState` nous n'en avons plus besoin
-import {useEffect, useState} from 'react'
-// 🐶 À la place, importe `use` from "react"
+import {getPosts} from '@/db/sgbd'
 
-// 🐶 Crée une fonction `fetchPosts`
-// 🤖
-// const fetchPosts = async () => {
-//   const response = await fetch('http://localhost:3000/exercises/api/posts')
-//   const data = await response.json()
-//   return data as Post[]
-// }
+const fetchPosts = async () => {
+  const response = await fetch('http://localhost:3000/exercises/api/posts')
+  const data = await response.json()
+  return data as Post[]
+}
 
-const Page = () => {
-  // ⛏️ Supprime ce state
-  const [posts, setPosts] = useState<Post[]>([])
-
-  // ⛏️ Supprime le `useEffect`
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch(
-          'http://localhost:3000/exercises/api/posts'
-        )
-        const data = await response.json()
-        setPosts(data as Post[])
-      } catch (error) {
-        console.error('Error fetching posts:', error)
-      }
-    }
-    fetchPosts()
-  }, [])
-
-  // 🐶 Appelle la fonction `fetchPosts()` dans `use()` et affecte le résultat dans "posts"
-  // 🐶 logue posts : console.log(posts)
-  // Constate que nous sommes toujours en train de fetch les posts dans un composants clients
+const Page = async () => {
+  // const posts = await fetchPosts()
+  const posts = await getPosts()
   return (
     <div className="mx-auto max-w-4xl p-6 text-lg">
       <h1 className="mb-4 text-center text-3xl font-bold"> Fetch Posts</h1>
